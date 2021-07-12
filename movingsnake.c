33835b9d4cf7a6123642ecea9a2f56fa01f97a26
max397574 <max.heinzer397@gmx.ch>
2021-07-12 10:37:14 +0200

    works now

--- movingsnake.c
+++ movingsnake.c
@@ -1,7 +1,7 @@
 #include <ncurses.h>
 #include <unistd.h>
 
-#define DELAY 30000
+#define DELAY 50000
 #define RIGHT 1
 #define DOWN 2
 #define LEFT 3
@@ -10,7 +10,7 @@ int i;
 int len=5;
 
 int main(int argc, char *argv[]) {
-    int x[200]={5};
+    int x[200]={5,5,5,5,5};
     int y[200]={5};
     int max_y = 0, max_x = 0;
     int next_x = 0;
@@ -25,6 +25,10 @@ int main(int argc, char *argv[]) {
 	clear();
 	for (i=0; i<len; i++) {
 	    mvprintw(y[i], x[i], "o");
+	}
+	for (i=len; i>0; i--) {
+	    y[i]=y[i-1];
+	    x[i]=x[i-1];
 	}
 	refresh();
 
